928891a0773f8bceb45bc6f5d692c1e00076dd9e
Josh Bassett <josh.bassett@gmail.com>
2014-09-21 15:33:11 +1000

    Include functions in mixed modules.

--- src/template.js
+++ src/template.js
@@ -11,24 +11,45 @@ var renderComponent = F.curry(function(component, child) {
 });
 
 var kind     = function(a) { return F.compose(F.eql(a), F.get('kind')); },
-    memberof = function(a) { return F.compose(F.eql(a), F.get('memberof')); };
+    memberOf = function(a) { return F.compose(F.eql(a), F.get('memberof')); };
+
+var isPublic = function(a) {
+  return a.access ? a.access !== 'private' : true;
+};
+
+var mixedInto = F.curry(function(a, b) {
+  return a.mixes ? F.elem(b.longname, a.mixes) : false;
+});
 
 var moduleWithKey = function(m) {
   return F.copy({key: 'module-' + m.name}, m);
 };
 
-function compareByName(a, b) {
-  return F.compare(a.name, b.name);
-}
+function compareByName(a, b) { return F.compare(a.name, b.name); }
 
-function parseModules(data) {
-  var modules = F.filter(kind('module'), data).sort(compareByName);
+// Returns functions which are members of a module.
+var moduleFunctions = F.curry(function(data, module) {
+  var p = F.whereAll([kind('function'), memberOf(module.longname)]);
+  return data.filter(p);
+});
+
+// Returns the modules which are mixed into a module.
+var moduleMixins = F.curry(function(data, module) {
+  var p = mixedInto(module);
+  return data.filter(p);
+});
+
+function buildModules(data) {
+  var modules = F.filter(F.whereAll([isPublic, kind('module')]), data).sort(compareByName);
 
   return modules.map(function(module) {
-    var p = F.whereAll([kind('function'), memberof(module.longname)]),
-        functions = data.filter(p).sort(compareByName);
+    var mixins = moduleMixins(data, module);
 
-    // Merge the functions into the module.
+    var functions = F
+      .concatMap(moduleFunctions(data), F.concat(module, mixins))
+      .sort(compareByName);
+
+    // Merge the functions into the module object.
     return F.copy({functions: functions}, module);
   });
 }
@@ -40,7 +61,7 @@ exports.render = function(db, opts) {
   db({undocumented: true}).remove();
 
   var data     = db().get(),
-      modules  = parseModules(data),
+      modules  = buildModules(data),
       html     = renderComponent(PageComponent, modules.map(F.compose(ModuleComponent, moduleWithKey))),
       doctype  = '<!DOCTYPE html>';
 
