95e958609d38feec9afc15572f12863bd9806fb4
Josh Bassett <josh.bassett@gmail.com>
2017-12-11 21:57:17 +1100

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,31 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin'),
-    path              = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-var exclude = /node_modules/;
+const exclude = /node_modules/
 
 module.exports = {
-  entry: {app: path.join(__dirname, 'src', 'app.js')},
+  entry: {app: __dirname + '/src/app.js'},
   output: {
-    path:     path.join(__dirname, 'build'),
+    path: __dirname + '/build',
     filename: '[name].js',
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   module: {
-    loaders: [
-      {test: /\.js$/, exclude: exclude, loader: 'babel'},
-      {test: /\.jsx$/, exclude: exclude, loaders: ['jsx', 'babel']},
-      {test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css!less')},
-      {test: /\.woff2?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
-      {test: /\.ttf$/,  loader: 'file'},
-      {test: /\.eot$/,  loader: 'file'},
-      {test: /\.svg$/,  loader: 'file'}
+    rules: [
+      {test: /\.js$/, exclude: exclude, use: 'babel-loader'},
+      {test: /\.jsx$/, exclude: exclude, use: ['jsx-loader', 'babel-loader']},
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'less-loader'],
+          fallback: 'style-loader'
+        })
+      },
+      {test: /\.woff2?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff'},
+      {test: /\.ttf$/, use: 'file-loader'},
+      {test: /\.eot$/, use: 'file-loader'},
+      {test: /\.svg$/, use: 'file-loader'}
     ]
   },
   plugins: [
